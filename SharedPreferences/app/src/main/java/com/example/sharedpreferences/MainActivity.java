5736edd18a3a5481654a1a715d72f53bd4e1af7a
carlosguinovart <103029861+carlosguinovart@users.noreply.github.com>
2023-05-17 08:48:38 +0200

    Commit SharedPreferences

--- SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.sharedpreferences;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etShared=findViewById(R.id.etShared);
-        sharedPreferences= getSharedPreferences("Preferencies", MODE_PRIVATE);
+        sharedPreferences= getSharedPreferences(getString(R.string.shared_file), MODE_PRIVATE);
+
+        String guardat = sharedPreferences.getString("textGuardat", "");
+
+        etShared.setText(guardat);
+        int contador = 0;
+
+        Toast.makeText(this, contador, Toast.LENGTH_SHORT).show();
 
     }
 
     public void savePreferences(View view) {
         String text = etShared.getText().toString();
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("textGuardat",text);
+        editor.commit();
+
+
+    }
+
+    public void openClicked(View view) {
+
+        Intent intent = new Intent(this, SegonaActivity.class);
+        startActivity(intent);
+
 
     }
 }
---
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/SegonaActivity.java
@@ -0,0 +1,32 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class SegonaActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segona);
+
+
+        prefs = getSharedPreferences(getString(R.string.shared_file), MODE_PRIVATE);
+
+        int contador = prefs.getInt("contador", 0);
+        contador ++;
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("Contador", contador);
+        editor.commit();
+
+
+
+
+    }
+}
